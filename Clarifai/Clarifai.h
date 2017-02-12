44949bfc48477f4e8a6ad298c561fef784160ab0
Dmitriy Koppel <Dimetrakop@users.noreply.github.com>
2017-02-12 22:09:24 +0200

    Add files via upload

---
+++ Clarifai/Clarifai.h
@@ -0,0 +1,19 @@
+//
+//  Clarifai.h
+//  Clarifai
+//
+//  Created by Dmitriy Koppel on 12.02.17.
+//  Copyright © 2017 Dmitriy Koppel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Clarifai.
+FOUNDATION_EXPORT double ClarifaiVersionNumber;
+
+//! Project version string for Clarifai.
+FOUNDATION_EXPORT const unsigned char ClarifaiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clarifai/PublicHeader.h>
+
+
